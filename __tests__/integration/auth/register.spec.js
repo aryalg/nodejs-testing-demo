d2b0129a4f431b62c0223882f49d0902b1d659ad
Bikram Aryal <bikramisdeveloper@gmail.com>
2020-04-29 22:40:13 +0545

    Testing failed validations

--- __tests__/integration/auth/register.spec.js
+++ __tests__/integration/auth/register.spec.js
@@ -13,21 +13,50 @@ const app = () => supertest(server)
 
 describe('The Register Process', () => {
 
+    const REGISTER_ENDPOINT = '/api/v1/auth/register'
+
+    let user = {
+        name: 'Test User',
+        email: 'test1@gmail.com',
+        password: 'kathmandu'
+    }
+
     beforeEach(async () => {
         await User.deleteMany({})
 
     })
     it('should register a new user', async () => {
-        const response = await app().post('/api/v1/auth/register').send({
-            name: 'Test User',
-            email: 'test1@gmail.com',
-            password: 'kathmandu'
-        })
+        const response = await app().post(REGISTER_ENDPOINT).send(user)
 
         expect(response.status).toBe(200);
         expect(response.body.message).toBe('Account registered.')
         expect(response.body.data.token).toBeDefined()
 
+    })
+
+
+    it('should return 422 if registration fails', async () => {
+        // prepration
+        const createdUser = await User.create(user)
+
+
+        // action
+        const response = await app().post(REGISTER_ENDPOINT).send(user)
+
+        // assertion
+        expect(response.status).toBe(422)
+
+        expect(response.body.message).toBe('Validation failed.')
+
+        expect(response.body.data.errors).toEqual({
+            email: 'This email has already been taken.'
+        })
+
+
+    })
+
+    afterAll(async () => {
         await disconnect()
+
     })
 })
